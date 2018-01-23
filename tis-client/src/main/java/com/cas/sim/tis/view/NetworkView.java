01471234dfab89f4c685c19650ee36d70675d204
张振宇 <sco_pra@126.com>
2018-01-23 12:48:25 +0800

    clean up

--- tis-client/src/main/java/com/cas/sim/tis/view/NetworkView.java
+++
@@ -1,8 +0,0 @@
-package com.cas.sim.tis.view;
-
-import de.felixroske.jfxsupport.AbstractFxmlView;
-import de.felixroske.jfxsupport.FXMLView;
-
-@FXMLView(value="/view/Network.fxml",bundle = "i18n.messages")
-public class NetworkView extends AbstractFxmlView {
-}
