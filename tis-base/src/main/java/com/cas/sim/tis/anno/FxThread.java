8c8dcf92b1878f3f39853072cc7094f614a43e7b
张振宇 <sco_pra@126.com>
2018-02-25 10:19:20 +0800

    自定义两个注解FXThread和JmeThread

---
+++ tis-base/src/main/java/com/cas/sim/tis/anno/FxThread.java
@@ -0,0 +1,17 @@
+package com.cas.sim.tis.anno;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * 标记一个方法应该在JavaFX的线程内执行
+ */
+@Inherited
+@Documented
+@Retention(RetentionPolicy.CLASS)
+@Target(ElementType.METHOD)
+public @interface FxThread {}
