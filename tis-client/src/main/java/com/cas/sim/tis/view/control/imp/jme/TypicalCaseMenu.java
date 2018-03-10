4fefcae986b68bb1b47e57511d9b6229bd172f40
caowj <151492350@qq.com>
2018-03-10 14:19:24 +0800

    新增、打开、保存图标；国际化

--- tis-client/src/main/java/com/cas/sim/tis/view/control/imp/jme/TypicalCaseMenu.java
+++ tis-client/src/main/java/com/cas/sim/tis/view/control/imp/jme/TypicalCaseMenu.java
@@ -43,14 +43,14 @@ public class TypicalCaseMenu implements ILeftContent {
 	public Region getLeftContent() {
 		VBox vb = new VBox();
 //		vb.getStyleClass().add("cover");
-		Label create = createMenu(MsgUtil.getMessage("menu.button.new"), new SVGGlyph("iconfont.svg.image", Color.WHITE, 16));
+		Label create = createMenu(MsgUtil.getMessage("menu.button.new"), new SVGGlyph("iconfont.svg.new", Color.WHITE, 16));
 //		create.setOnMouseClicked(e -> showDrawDialog());
 		create.setOnMouseClicked(e -> newCase());
 
-		Label open = createMenu(MsgUtil.getMessage("menu.button.open"), new SVGGlyph("iconfont.svg.setting", Color.WHITE, 16));
+		Label open = createMenu(MsgUtil.getMessage("menu.button.open"), new SVGGlyph("iconfont.svg.open", Color.WHITE, 16));
 		open.setOnMouseClicked(e -> showCaseDialog());
 
-		Label save = createMenu(MsgUtil.getMessage("menu.button.save"), new SVGGlyph("iconfont.svg.setting", Color.WHITE, 16));
+		Label save = createMenu(MsgUtil.getMessage("menu.button.save"), new SVGGlyph("iconfont.svg.save", Color.WHITE, 16));
 		save.setOnMouseClicked(e -> saveCase());
 
 		HBox menu = new HBox(15, create, open, save);
