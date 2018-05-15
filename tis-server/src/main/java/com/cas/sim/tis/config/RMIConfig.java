7ffb4280ffc1331a4c4995ea38882aa5cf9071a9
张振宇 <sco_pra@126.com>
2018-05-15 19:32:01 +0800

    修改RMI服务注册方式
    Signed-off-by: 张振宇 <sco_pra@126.com>

--- tis-server/src/main/java/com/cas/sim/tis/config/RMIConfig.java
+++ tis-server/src/main/java/com/cas/sim/tis/config/RMIConfig.java
@@ -83,311 +83,202 @@ public class RMIConfig {
 	@Value("${server.rmi.service}")
 	private Integer servicePort;
 
-	@Bean
-	public RmiServiceExporter userServiceExporter() {
+	private RmiServiceExporter getRmiServiceExporter() {
 		RmiServiceExporter exporter = new RmiServiceExporter();
-		exporter.setServiceInterface(UserService.class);
-		exporter.setServiceName("userService");
-		exporter.setService(userService);
-
+//		publicNetwork
+//		exporter.setRegistryHost(publicNetwork);
 		exporter.setServicePort(servicePort);
 //		RegistryPort端口默认为1099，用户客户端访问时用的，rmi://host:RegistryPort/serviceName
 		exporter.setRegistryPort(registPort);
+		return exporter;
+	}
+
+	@Bean
+	public RmiServiceExporter userServiceExporter() {
+		RmiServiceExporter exporter = getRmiServiceExporter();
+		exporter.setServiceInterface(UserService.class);
+		exporter.setServiceName("userService");
+		exporter.setService(userService);
 		return exporter;
 	}
 
 	@Bean
 	public RmiServiceExporter classServiceExporter() {
-		RmiServiceExporter exporter = new RmiServiceExporter();
+		RmiServiceExporter exporter = getRmiServiceExporter();
 		exporter.setServiceInterface(ClassService.class);
 		exporter.setServiceName("classService");
 		exporter.setService(classService);
-
-		exporter.setServicePort(servicePort);
-		exporter.setRegistryPort(registPort);
 		return exporter;
 	}
 
 	@Bean
 	public RmiServiceExporter resourceServiceExporter() {
-		RmiServiceExporter exporter = new RmiServiceExporter();
+		RmiServiceExporter exporter = getRmiServiceExporter();
 		exporter.setServiceInterface(ResourceService.class);
 		exporter.setServiceName("resourceService");
 		exporter.setService(resourceService);
-
-		exporter.setServicePort(servicePort);
-		exporter.setRegistryPort(registPort);
 		return exporter;
 	}
 
 	@Bean
 	public RmiServiceExporter collectionServiceExporter() {
-		RmiServiceExporter exporter = new RmiServiceExporter();
+		RmiServiceExporter exporter = getRmiServiceExporter();
 		exporter.setServiceInterface(CollectionService.class);
 		exporter.setServiceName("collectionService");
 		exporter.setService(collectionService);
-
-		exporter.setServicePort(servicePort);
-		exporter.setRegistryPort(registPort);
 		return exporter;
 	}
 
 	@Bean
 	public RmiServiceExporter drawServiceExporter() {
-		RmiServiceExporter exporter = new RmiServiceExporter();
+		RmiServiceExporter exporter = getRmiServiceExporter();
 		exporter.setServiceInterface(DrawService.class);
 		exporter.setServiceName("drawService");
 		exporter.setService(drawService);
-
-		exporter.setServicePort(servicePort);
-		exporter.setRegistryPort(registPort);
 		return exporter;
 	}
 
 	@Bean
 	public RmiServiceExporter elecCompServiceExporter() {
-		RmiServiceExporter exporter = new RmiServiceExporter();
+		RmiServiceExporter exporter = getRmiServiceExporter();
 		exporter.setServiceInterface(ElecCompService.class);
 		exporter.setServiceName("elecCompService");
 		exporter.setService(elecCompService);
-
-		exporter.setServicePort(servicePort);
-		exporter.setRegistryPort(registPort);
 		return exporter;
 	}
 
 	@Bean
 	public RmiServiceExporter typicalCaseServiceExporter() {
-		RmiServiceExporter exporter = new RmiServiceExporter();
+		RmiServiceExporter exporter = getRmiServiceExporter();
 		exporter.setServiceInterface(TypicalCaseService.class);
 		exporter.setServiceName("typicalCaseService");
 		exporter.setService(typicalCaseService);
-
-		exporter.setServicePort(servicePort);
-		exporter.setRegistryPort(registPort);
 		return exporter;
 	}
 
 	@Bean
 	public RmiServiceExporter brokenCaseServiceExporter() {
-		RmiServiceExporter exporter = new RmiServiceExporter();
+		RmiServiceExporter exporter = getRmiServiceExporter();
 		exporter.setServiceInterface(BrokenCaseService.class);
 		exporter.setServiceName("brokenCaseService");
 		exporter.setService(brokenCaseService);
-
-		exporter.setServicePort(servicePort);
-		exporter.setRegistryPort(registPort);
 		return exporter;
 	}
 
 	@Bean
 	public RmiServiceExporter browseHistoryServiceExporter() {
-		RmiServiceExporter exporter = new RmiServiceExporter();
+		RmiServiceExporter exporter = getRmiServiceExporter();
 		exporter.setServiceInterface(BrowseHistoryService.class);
 		exporter.setServiceName("browseHistoryService");
 		exporter.setService(browseHistoryService);
-
-		if (servicePort != null) {
-			exporter.setServicePort(servicePort);
-		}
-//		RegistryPort端口默认为1099，用户客户端访问时用的，rmi://host:RegistryPort/serviceName
-		if (registPort != null) {
-			exporter.setRegistryPort(registPort);
-		}
 		return exporter;
 	}
 
 	@Bean
 	public RmiServiceExporter libraryServiceExporter() {
-		RmiServiceExporter exporter = new RmiServiceExporter();
+		RmiServiceExporter exporter = getRmiServiceExporter();
 		exporter.setServiceInterface(LibraryService.class);
 		exporter.setServiceName("libraryService");
 		exporter.setService(libraryService);
-
-		if (servicePort != null) {
-			exporter.setServicePort(servicePort);
-		}
-//		RegistryPort端口默认为1099，用户客户端访问时用的，rmi://host:RegistryPort/serviceName
-		if (registPort != null) {
-			exporter.setRegistryPort(registPort);
-		}
 		return exporter;
 	}
 
 	@Bean
 	public RmiServiceExporter questionServiceExporter() {
-		RmiServiceExporter exporter = new RmiServiceExporter();
+		RmiServiceExporter exporter = getRmiServiceExporter();
 		exporter.setServiceInterface(QuestionService.class);
 		exporter.setServiceName("questionService");
 		exporter.setService(questionService);
-
-		if (servicePort != null) {
-			exporter.setServicePort(servicePort);
-		}
-//		RegistryPort端口默认为1099，用户客户端访问时用的，rmi://host:RegistryPort/serviceName
-		if (registPort != null) {
-			exporter.setRegistryPort(registPort);
-		}
 		return exporter;
 	}
 
 	@Bean
 	public RmiServiceExporter libraryPublishServiceExporter() {
-		RmiServiceExporter exporter = new RmiServiceExporter();
+		RmiServiceExporter exporter = getRmiServiceExporter();
 		exporter.setServiceInterface(LibraryPublishService.class);
 		exporter.setServiceName("libraryPublishService");
 		exporter.setService(libraryPublishService);
-
-		if (servicePort != null) {
-			exporter.setServicePort(servicePort);
-		}
-		if (registPort != null) {
-			exporter.setRegistryPort(registPort);
-		}
 		return exporter;
 	}
 
 	@Bean
 	public RmiServiceExporter libraryRecordServiceExporter() {
-		RmiServiceExporter exporter = new RmiServiceExporter();
+		RmiServiceExporter exporter = getRmiServiceExporter();
 		exporter.setServiceInterface(LibraryRecordService.class);
 		exporter.setServiceName("libraryRecordService");
 		exporter.setService(libraryRecordService);
-
-		if (servicePort != null) {
-			exporter.setServicePort(servicePort);
-		}
-		if (registPort != null) {
-			exporter.setRegistryPort(registPort);
-		}
 		return exporter;
 	}
 
 	@Bean
 	public RmiServiceExporter libraryAnswerServiceExporter() {
-		RmiServiceExporter exporter = new RmiServiceExporter();
+		RmiServiceExporter exporter = getRmiServiceExporter();
 		exporter.setServiceInterface(LibraryAnswerService.class);
 		exporter.setServiceName("libraryAnswerService");
 		exporter.setService(libraryAnswerService);
-
-		if (servicePort != null) {
-			exporter.setServicePort(servicePort);
-		}
-		if (registPort != null) {
-			exporter.setRegistryPort(registPort);
-		}
 		return exporter;
 	}
 
 	@Bean
 	public RmiServiceExporter catalogServiceExporter() {
-		RmiServiceExporter exporter = new RmiServiceExporter();
+		RmiServiceExporter exporter = getRmiServiceExporter();
 		exporter.setServiceInterface(CatalogService.class);
 		exporter.setServiceName("catalogService");
 		exporter.setService(catalogService);
-
-		if (servicePort != null) {
-			exporter.setServicePort(servicePort);
-		}
-		if (registPort != null) {
-			exporter.setRegistryPort(registPort);
-		}
 		return exporter;
 	}
 
 	@Bean
 	public RmiServiceExporter preparationServiceExporter() {
-		RmiServiceExporter exporter = new RmiServiceExporter();
+		RmiServiceExporter exporter = getRmiServiceExporter();
 		exporter.setServiceInterface(PreparationService.class);
 		exporter.setServiceName("preparationService");
 		exporter.setService(preparationService);
-
-		if (servicePort != null) {
-			exporter.setServicePort(servicePort);
-		}
-		if (registPort != null) {
-			exporter.setRegistryPort(registPort);
-		}
 		return exporter;
 	}
 
 	@Bean
 	public RmiServiceExporter preparationResourceServiceExporter() {
-		RmiServiceExporter exporter = new RmiServiceExporter();
+		RmiServiceExporter exporter = getRmiServiceExporter();
 		exporter.setServiceInterface(PreparationResourceService.class);
 		exporter.setServiceName("preparationResourceService");
 		exporter.setService(preparationResourceService);
-
-		if (servicePort != null) {
-			exporter.setServicePort(servicePort);
-		}
-		if (registPort != null) {
-			exporter.setRegistryPort(registPort);
-		}
 		return exporter;
 	}
 
 	@Bean
 	public RmiServiceExporter preparationQuizServiceExporter() {
-		RmiServiceExporter exporter = new RmiServiceExporter();
+		RmiServiceExporter exporter = getRmiServiceExporter();
 		exporter.setServiceInterface(PreparationQuizService.class);
 		exporter.setServiceName("preparationQuizService");
 		exporter.setService(preparationQuizService);
-
-		if (servicePort != null) {
-			exporter.setServicePort(servicePort);
-		}
-		if (registPort != null) {
-			exporter.setRegistryPort(registPort);
-		}
 		return exporter;
 	}
-	
+
 	@Bean
 	public RmiServiceExporter goalServiceExporter() {
-		RmiServiceExporter exporter = new RmiServiceExporter();
+		RmiServiceExporter exporter = getRmiServiceExporter();
 		exporter.setServiceInterface(GoalService.class);
 		exporter.setServiceName("goalService");
 		exporter.setService(goalService);
-
-		if (servicePort != null) {
-			exporter.setServicePort(servicePort);
-		}
-		if (registPort != null) {
-			exporter.setRegistryPort(registPort);
-		}
 		return exporter;
 	}
-	
+
 	@Bean
 	public RmiServiceExporter goalRelationshipServiceExporter() {
-		RmiServiceExporter exporter = new RmiServiceExporter();
+		RmiServiceExporter exporter = getRmiServiceExporter();
 		exporter.setServiceInterface(GoalRelationshipService.class);
 		exporter.setServiceName("goalRelationshipService");
 		exporter.setService(goalRelationshipService);
-
-		if (servicePort != null) {
-			exporter.setServicePort(servicePort);
-		}
-		if (registPort != null) {
-			exporter.setRegistryPort(registPort);
-		}
 		return exporter;
 	}
-	
+
 	@Bean
 	public RmiServiceExporter goalCoverageServiceExporter() {
-		RmiServiceExporter exporter = new RmiServiceExporter();
+		RmiServiceExporter exporter = getRmiServiceExporter();
 		exporter.setServiceInterface(GoalCoverageService.class);
 		exporter.setServiceName("goalCoverageService");
 		exporter.setService(goalCoverageService);
-		
-		if (servicePort != null) {
-			exporter.setServicePort(servicePort);
-		}
-		if (registPort != null) {
-			exporter.setRegistryPort(registPort);
-		}
 		return exporter;
 	}
 }
