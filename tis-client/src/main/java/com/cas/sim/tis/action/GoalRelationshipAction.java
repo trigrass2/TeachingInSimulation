d8941da292b949e99caaaee0eb91e933f0f69461
张振宇 <sco_pra@126.com>
2018-05-16 09:23:08 +0800

    RMI远程对象注入

--- tis-client/src/main/java/com/cas/sim/tis/action/GoalRelationshipAction.java
+++ tis-client/src/main/java/com/cas/sim/tis/action/GoalRelationshipAction.java
@@ -2,13 +2,14 @@ package com.cas.sim.tis.action;
 
 import javax.annotation.Resource;
 
-import org.springframework.remoting.rmi.RmiProxyFactoryBean;
 import org.springframework.stereotype.Component;
+
+import com.cas.sim.tis.services.GoalRelationshipService;
 
 @Component
 public class GoalRelationshipAction extends BaseAction {
 
 	@Resource(name = "goalRelationshipService")
-	private RmiProxyFactoryBean service;
+	private GoalRelationshipService service;
 
 }
