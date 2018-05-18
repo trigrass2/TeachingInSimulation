3fb9142686f708d8f2219831a1e9e9ce8df76529
张振宇 <sco_pra@126.com>
2018-05-18 13:20:01 +0800

    封装请求与响应消息

--- tis-base/src/main/java/com/cas/sim/tis/thrift/RequestEntity.java
+++ tis-base/src/main/java/com/cas/sim/tis/thrift/RequestEntity.java
@@ -1,5 +1,8 @@
 package com.cas.sim.tis.thrift;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+
 import io.airlift.drift.annotations.ThriftField;
 import io.airlift.drift.annotations.ThriftStruct;
 import lombok.EqualsAndHashCode;
@@ -21,4 +24,40 @@ public class RequestEntity {
 //	每页的数量
 	@ThriftField(3)
 	public int pageSize = 10;
+
+	private JSONObject json;
+
+	public RequestEntity set(String key, Object value) {
+		if (json == null) {
+			json = new JSONObject();
+		}
+		json.put(key, value);
+		return this;
+	}
+
+	public void end() {
+		this.data = json.toJSONString();
+	}
+
+	public Integer getInt(String key) {
+		if (json == null) {
+			json = JSON.parseObject(data);
+		}
+		return json.getInteger(key);
+	}
+
+	public String getString(String key) {
+		if (json == null) {
+			json = JSON.parseObject(data);
+		}
+		return json.getString(key);
+	}
+
+	public Object getObject(String key) {
+		if (json == null) {
+			json = JSON.parseObject(data);
+		}
+		return json.get(key);
+	}
+
 }
--- tis-base/src/main/java/com/cas/sim/tis/thrift/ResponseEntity.java
+++ tis-base/src/main/java/com/cas/sim/tis/thrift/ResponseEntity.java
@@ -1,5 +1,7 @@
 package com.cas.sim.tis.thrift;
 
+import com.alibaba.fastjson.JSON;
+
 import io.airlift.drift.annotations.ThriftField;
 import io.airlift.drift.annotations.ThriftStruct;
 import lombok.EqualsAndHashCode;
@@ -29,4 +31,20 @@ public class ResponseEntity {
 	 * 返回数据
 	 */
 	public String data;
+
+	public static ResponseEntity success(Object data) {
+		ResponseEntity entity = new ResponseEntity();
+		entity.code = ResponseEntity.SUCCESS;
+		entity.msg = "OK";
+		entity.data = JSON.toJSONString(data);
+		return entity;
+	}
+
+	public static ResponseEntity failure(String msg) {
+		ResponseEntity entity = new ResponseEntity();
+		entity.code = ResponseEntity.FAILURE;
+		entity.msg = msg;
+		entity.data = "";
+		return entity;
+	}
 }
