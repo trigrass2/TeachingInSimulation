4a43611c6c310801444c02ac1aa6fbd9dcd105b3
caowj <151492350@qq.com>
2018-05-19 13:14:56 +0800

    注释

--- tis-server/src/main/java/com/cas/sim/tis/mapper/ClassMapper.java
+++ tis-server/src/main/java/com/cas/sim/tis/mapper/ClassMapper.java
@@ -10,9 +10,17 @@ import com.cas.sim.tis.vo.ClassInfo;
 
 @Mapper
 public interface ClassMapper extends IMapper<Class> {
-
+	/**
+	 * 根据导入信息新增班级信息（通过教师工号获得用户信息编号）
+	 * @param infos
+	 * @param creator
+	 */
 	void insertClasses(@Param("infos") List<ClassInfo> infos, @Param("creator") Integer creator);
 
+	/**
+	 * 查询班级信息并关联负责教师信息
+	 * @return
+	 */
 	List<Class> findClasses();
 
 }
