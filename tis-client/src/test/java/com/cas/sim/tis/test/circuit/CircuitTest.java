bdd610252303c7d86f1d40ff5c906f26fc557e4f
张振宇 <sco_pra@126.com>
2018-05-19 19:41:27 +0800

    修改一些测试类

--- tis-client/src/test/java/com/cas/sim/tis/test/circuit/CircuitTest.java
+++ tis-client/src/test/java/com/cas/sim/tis/test/circuit/CircuitTest.java
@@ -6,15 +6,13 @@ import javax.annotation.Resource;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.remoting.rmi.RmiProxyFactoryBean;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.cas.circuit.vo.ElecCompDef;
 import com.cas.sim.tis.Application;
+import com.cas.sim.tis.action.ElecCompAction;
 import com.cas.sim.tis.entity.ElecComp;
-import com.cas.sim.tis.services.ElecCompService;
 import com.cas.sim.tis.util.HTTPUtils;
 import com.cas.sim.tis.xml.util.JaxbUtil;
 
@@ -23,16 +21,14 @@ import com.cas.sim.tis.xml.util.JaxbUtil;
 public class CircuitTest {
 
 	@Resource
-	@Qualifier(value = "elecCompServiceFactory")
-	private RmiProxyFactoryBean elecCompServiceFactory;
+	ElecCompAction action;
 
 	@Test
 	public void testParseCfg() throws Exception {
-		ElecCompService service = (ElecCompService) elecCompServiceFactory.getObject();
 //		ID:10\11
 //		ElecComp elecComp = service.findById(10);
-		ElecComp elecComp = service.findBy("model", "CJX2-12");
-
+		ElecComp elecComp = action.getElecComp("CJX2-1210");
+		System.out.println(elecComp);
 		String cfgPath = HTTPUtils.getFullPath(elecComp.getCfgPath());
 		URL url = new URL(cfgPath);
 		ElecCompDef elecCompDef = JaxbUtil.converyToJavaBean(url, ElecCompDef.class);
